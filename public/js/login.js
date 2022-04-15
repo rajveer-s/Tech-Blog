e3895d9f259daa66fab9692da2d2e81236d0d8ca
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-14 22:33:28 -0700

    public/js logout added

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ async function loginFormHandler(event) {
 
   if (username && password) {
     console.log(username)
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({
         username,
