61d69ab793c901508df01af02d67cbe4f8c55faf
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-14 22:30:59 -0700

    api postRoutes added

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ['comment_data'],
+        },
+      ],
+    });
+
+    res.status(200).json(postData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newitem = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newitem);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const itemData = await Item.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!itemData) {
+      res.status(404).json({ message: 'No Item found with this id!' });
+      return;
+    }
+
+    res.status(200).json(itemData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
