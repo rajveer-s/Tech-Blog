37d30c4d1a37e33c69ff73f6b8f858473897f7b3
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-18 09:40:25 -0700

    deploy aa

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postroutes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./comment');
 
 router.use('/users', userRoutes);
