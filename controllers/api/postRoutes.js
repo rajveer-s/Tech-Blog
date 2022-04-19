bd97ce8241e530c3867abffb1d6965de9b5a30ce
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-18 19:01:25 -0700

    Post put route made/ css for buttons done

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -51,6 +51,18 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
