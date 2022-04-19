d8731df69cf5c06c841fc97a35cfff37da72a6a0
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-18 18:52:29 -0700

    edit/delete functions made

---
+++ public/js/edit.js
@@ -0,0 +1,61 @@
+async function editForm(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('#postTitle').value.trim();
+  const content = document.querySelector(
+    '#postDes').value.trim();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      id,
+      title,
+      content
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+
+}
+
+
+async function deleteForm(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+
+}
+
+
+
+document.querySelector('#editPost').addEventListener('click', editForm);
+document.querySelector('#deletePost').addEventListener('click', deleteForm);
